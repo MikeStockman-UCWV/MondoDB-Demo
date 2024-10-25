8d4ce03bf77c20c86942e87cf396754e698f646f
Mike Stockman <michaelstockman@ucwv.edu>
2024-10-25 07:25:37 -0400

    Update server.js
    this updates server to ensure app is set to listen on all available network interfaces

--- server.js
+++ server.js
@@ -65,6 +65,10 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+//app.listen(3000, () => {
+//  console.log('Server running on http://localhost:3000');
+//});
+app.listen(3000, '0.0.0.0', () => {
+  console.log("Server is running on port 3000");
 });
+
